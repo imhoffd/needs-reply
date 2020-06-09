24255aee7e28fea27b1de23b70e8dca96fc7ee07
Daniel Imhoff <dwieeb@gmail.com>
2020-06-09 16:22:22 -0700

    print token

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+import * as core from '@actions/core';
+
+const run = async (): Promise<void> => {
+  const repoToken = core.getInput('repo-token', { required: true });
+
+  console.log(`Token: ${repoToken}`);
+};
+
+run();
